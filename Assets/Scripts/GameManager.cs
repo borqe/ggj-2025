e888118dd12dda255a4c8c8a705cc9882bba416c
Robertas Paulauskas <r.paulauskas@protonmail.com>
2025-01-25 19:37:17 +0200

    pause menu, intro music

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,6 +7,9 @@ public class GameManager : MonoBehaviour
     private static GameManager instance;
     
     [SerializeField] private GameObject loadingScreen;
+    [SerializeField] private GameObject pauseScreen;
+    
+    private bool isGamePaused = false;
 
     private void Awake()
     {
@@ -25,14 +27,64 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         loadingScreen.SetActive(false);
+        pauseScreen.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+            return;
+        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isGamePaused)
+            {
+                UnpauseGame();
+                isGamePaused = false;
+            }
+            else
+            {
+                PauseGame();
+                isGamePaused = true;
+            }
+        }
     }
 
     public void StartGame()
     {
         loadingScreen.SetActive(true);
-        
         SceneManager.LoadScene("TestScene");
-        
         loadingScreen.SetActive(false);
+        pauseScreen.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void BackToMainMenu()
+    {
+        loadingScreen.SetActive(true);
+        SceneManager.LoadScene("Boot");
+        loadingScreen.SetActive(false);
+        pauseScreen.SetActive(false);
+    }
+
+    public void PauseGame()
+    {
+        pauseScreen.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void UnpauseGame()
+    {
+        pauseScreen.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_STANDALONE && !UNITY_EDITOR
+        Application.Quit();
+#elif UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
