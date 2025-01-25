61200d40d5f9027249d32b8a3df65344dc7b9eeb
Robertas Paulauskas <r.paulauskas@protonmail.com>
2025-01-25 12:50:55 +0200

    add shooting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Transform gunTransform;
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] float bulletSpeed = 10f;
+    [SerializeField] float bulletLifeTime = 5f;
+    
 
     private bool isMoving;
     private bool isRolling;
@@ -21,36 +25,36 @@ public class PlayerController : MonoBehaviour
         aimDirection = mousePos - transform.position;
         float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
         gunTransform.rotation = Quaternion.Euler(0f, 0f, angle);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            var bullet = Instantiate(bulletPrefab, Vector3.zero, Quaternion.identity);
+            bullet.GetComponent<Rigidbody2D>().velocity = aimDirection.normalized * bulletSpeed;
+        }
         
         Vector2 moveDirection = Vector2.zero;
-
         if (Input.GetKey(KeyCode.W))
         {
             moveDirection += Vector2.up;
         }
-
         if (Input.GetKey(KeyCode.A))
         {
             moveDirection += Vector2.left;
         }
-
         if (Input.GetKey(KeyCode.S))
         {
             moveDirection += Vector2.down;
         }
-        
         if (Input.GetKey(KeyCode.D))
         {
             moveDirection += Vector2.right;
         }
-        
         if (Input.GetKeyDown(KeyCode.Space))
         {
             // TODO: teleporting atm, but will be fine
             transform.Translate(moveDirection.normalized * 3.0f);
             return;
         }
-        
         transform.Translate(moveDirection.normalized * (Time.deltaTime * moveSpeed));
     }
 }
